0af272ae65a72a6484479ca3e6d7fde630ec29a3
filipeafonso1710 <a22304588@alunos.ulht.pt>
2025-03-18 20:30:50 +0000

    teste

--- app/db/utils.go
+++ app/db/utils.go
@@ -104,6 +104,7 @@ func Populate() {
 		{0, "No Man’s Sky", "A space exploration game with infinite planets.", "Exploration"},
 		{0, "Hades", "A rogue-like dungeon crawler.", "Rogue-like"},
 		{0, "Disco Elysium", "A deep, story-driven RPG.", "RPG"},
+		{0, "Arroz Frito", "sla, faz arroz", "RPG, action, puzzle, horror"},
 	}
 
 	for _, g := range games {
